f623c000a6fc21c58d2e563cf231c478086d0b43
onodaHiroo <108873914+onodaHiroo@users.noreply.github.com>
2023-06-27 15:57:19 +0300

    Added music volume settings in maun menu and saving iin the level.

---
+++ Uproot/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    public Toggle toggleMusic;
+    public Slider sliderVolumeMusic;
+    public AudioSource audio;
+    public float volume;
+    public GameObject SettingsWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+        ValueMusic();
+
+        if (SettingsWindow.activeSelf == true)
+        {
+            toggleMusic = GameObject.FindGameObjectWithTag("Music Toggle").GetComponent<Toggle>();
+            sliderVolumeMusic = GameObject.FindGameObjectWithTag("Music Slider").GetComponent<Slider>();
+        }
+        
+    }
+
+   public void SliderMusic()
+    {
+        volume = sliderVolumeMusic.value;
+        Save();
+        ValueMusic();
+    }
+
+    public void ToggleMusic()
+    {
+        if (toggleMusic.isOn)
+        {
+            volume = 1;
+        }
+        else
+        {
+            volume = 0;
+        }
+        Save();
+        ValueMusic();
+    }
+
+    private void ValueMusic()
+    {
+        audio.volume = volume;
+        sliderVolumeMusic.value = volume;
+        if (volume == 0) { toggleMusic.isOn= false; } else { toggleMusic.isOn= true; }
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+    private void Load()
+    {
+        volume = PlayerPrefs.GetFloat("volume", volume);
+    }
+}
